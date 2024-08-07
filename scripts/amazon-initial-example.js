85429e7bd7ebcf9bfb982fe175ebe9b9740fa4e0
Said, Ali <Ali.Said@wolterskluwer.com>
2024-08-07 16:16:31 +0200

    Use products.js file

--- scripts/amazon-initial-example.js
+++ scripts/amazon-initial-example.js
@@ -1,3 +1,45 @@
+const products = [{
+  image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+  rating: {
+    stars: 4.5,
+    count: 87
+  },
+  priceCents: 1090
+}, {
+  image: 'images/products/intermediate-composite-basketball.jpg',
+  name: 'Intermediate Size Basketball',
+  rating: {
+    stars: 4,
+    count: 127
+  },
+  priceCents: 2095
+}, {
+  image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+  name: 'Adults Plain Cotton T-Shirt - 2 Pack',
+  rating: {
+    stars: 4.5,
+    count: 56
+  },
+  priceCents: 799
+}, {
+  image: 'images/products/black-2-slot-toaster.jpg',
+  name: '2 Slot Toaster - Black',
+  rating: {
+    stars: 5,
+    count: 2197
+  },
+  priceCents: 1899
+}, {
+  image: 'images/products/6-piece-white-dinner-plate-set.jpg',
+  name: '6 Piece White Dinner Plate Set',
+  rating: {
+    stars: 4,
+    count: 37
+  },
+  priceCents: 2067
+}];
+
 //1. saving data (info) with creating a list of objects
 //2. generate HTML with looping these objects
 //forEach Obj we generate an HTML
@@ -7,10 +49,10 @@
 
 //we are adding html in productsHTML EACH TIME we go through the loop
 
-let productsHTML = '';
+let productsHTML1 = '';
 
 products.forEach((product) => {
-  productsHTML += `
+  productsHTML1 += `
     <div class="product-container">
       <div class="product-image-container">
         <img class="product-image"
@@ -61,7 +103,7 @@ products.forEach((product) => {
     </div>
   `;
 });
-console.log(productsHTML);
+console.log(productsHTML1);
 
 document.querySelector('.js-products-grid')
-  .innerHTML = productsHTML;//we changed the html into productsHTML
+  .innerHTML = productsHTML1;//we changed the html into productsHTML
